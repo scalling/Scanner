f081f18ea1342b3db348281395aa82b51bd39d98
zengmei <1029540864@qq.com>
2018-11-30 17:37:25 +0800

    扫一扫，扫描手机号码，扫描条形码，扫描二维码

---
+++ app/src/main/java/com/zm/scanner/MainActivity.java
@@ -0,0 +1,157 @@
+package com.zm.scanner;
+
+import android.Manifest;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.tbruyelle.rxpermissions2.RxPermissions;
+import com.zm.scanner.view.PhoneZXingView;
+
+import cn.bingoogolapple.qrcode.core.BGAQRCodeUtil;
+import cn.bingoogolapple.qrcode.core.QRCodeView;
+import io.reactivex.functions.Consumer;
+
+public class MainActivity extends AppCompatActivity implements QRCodeView.Delegate, View.OnClickListener {
+
+    PhoneZXingView mQRCodeView;
+    EditText etExpressNoData;
+    EditText etPhoneNumberData;
+    TextView tvExpressNo;
+    TextView tvPhoneNumber;
+    TextView tvLight;
+    boolean isLight = false;
+    final RxPermissions rxPermissions = new RxPermissions(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BGAQRCodeUtil.setDebug(true);
+        initView();
+        initClick();
+        selectExpressNo();
+
+    }
+
+    private void initView() {
+        mQRCodeView = findViewById(R.id.my_zxing_view);
+        etExpressNoData = findViewById(R.id.et_express_no_data);
+        etPhoneNumberData = findViewById(R.id.et_phone_number_data);
+        tvExpressNo = findViewById(R.id.tv_express_no);
+        tvPhoneNumber = findViewById(R.id.tv_phone_number);
+        tvLight = findViewById(R.id.tv_light);
+    }
+
+    private void initClick() {
+        tvLight.setOnClickListener(this);
+        tvExpressNo.setOnClickListener(this);
+        tvPhoneNumber.setOnClickListener(this);
+        mQRCodeView.setDelegate(this);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startCamera();
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mQRCodeView.stopCamera();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mQRCodeView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onScanQRCodeSuccess(String result) {
+        if (mQRCodeView.isScannerPhone()) {
+            etPhoneNumberData.setText(result);
+        } else {
+            etExpressNoData.setText(result);
+        }
+        mQRCodeView.startSpotAndShowRect();
+    }
+
+    @Override
+    public void onCameraAmbientBrightnessChanged(boolean isDark) {
+
+    }
+
+
+    @Override
+    public void onScanQRCodeOpenCameraError() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_light:
+                isLight = !isLight;
+                if (isLight) {
+                    mQRCodeView.openFlashlight(); // 打开闪光灯
+                    tvLight.setTextColor(Color.BLUE);
+                } else {
+                    mQRCodeView.closeFlashlight(); // 关闭闪光灯
+                    tvLight.setTextColor(Color.WHITE);
+                }
+                break;
+            case R.id.tv_express_no:
+                selectExpressNo();
+                mQRCodeView.startSpotAndShowRect();
+                break;
+            case R.id.tv_phone_number:
+                selectPhoneNumber();
+                mQRCodeView.startSpotAndShowRect();
+                break;
+        }
+    }
+
+    private void selectExpressNo() {
+        mQRCodeView.getScanBoxView().setTopOffset(getResources().getDimensionPixelOffset(R.dimen.scan_express_top));
+        mQRCodeView.getScanBoxView().setBarcodeRectHeight(getResources().getDimensionPixelOffset(R.dimen.scan_express_height));
+        mQRCodeView.getScanBoxView().setOnlyDecodeScanBoxArea(false);
+        mQRCodeView.setScannerPhone(false);
+        tvExpressNo.setTextColor(Color.BLUE);
+        tvPhoneNumber.setTextColor(Color.WHITE);
+    }
+
+    private void selectPhoneNumber() {
+        mQRCodeView.getScanBoxView().setTopOffset(getResources().getDimensionPixelOffset(R.dimen.scan_phone_top));
+        mQRCodeView.getScanBoxView().setBarcodeRectHeight(getResources().getDimensionPixelOffset(R.dimen.scan_phone_height));
+        mQRCodeView.getScanBoxView().setOnlyDecodeScanBoxArea(true);
+        mQRCodeView.setScannerPhone(true);
+        tvPhoneNumber.setTextColor(Color.BLUE);
+        tvExpressNo.setTextColor(Color.WHITE);
+    }
+
+    /**
+     * 打开扫码
+     */
+    private void startCamera() {
+        rxPermissions
+                .request(Manifest.permission.CAMERA,
+                        Manifest.permission.READ_PHONE_STATE)
+                .subscribe(new Consumer<Boolean>() {
+                    @Override
+                    public void accept(Boolean aBoolean) throws Exception {
+                        if (aBoolean) {
+                            mQRCodeView.startCamera();
+                            mQRCodeView.startSpotAndShowRect();
+                        }
+                    }
+                });
+
+    }
+}
